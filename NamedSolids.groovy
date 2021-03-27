0123c90e2a309608a55979e3ba054492a23be566
BFTwChees <64285036+BFTwChees@users.noreply.github.com>
2021-03-27 12:08:48 -0400

    I changed the size and position of each shape
    I wanted to get familiar with bowler studio

--- NamedSolids.groovy
+++ NamedSolids.groovy
@@ -1,8 +1,11 @@
-double size =20
+double size1 =100
+double size2  =20
+double size3 =50
+double size4 =70
 return [
-new Dodecahedron(size).toCSG().movex(size*0),
-new Icosahedron(size).toCSG().movex(size*2),
-new Octahedron(size).toCSG().movex(size*4),
-new Tetrahedron(size).toCSG().movex(size*6),
+new Dodecahedron(100).toCSG().movex(size1*0),
+new Icosahedron(size2).toCSG().movex(size1+size2/2),
+new Octahedron(size3).toCSG().movex(size1+size2+size3/2),
+new Tetrahedron(size4).toCSG().movex(size1+size2+size3+size4/2)
 
 ]
